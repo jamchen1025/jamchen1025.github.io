c74d97809c3769eaab2c7a6a46515d9b061381c2
jam.chen <jam.chen1025@gmail.com>
2025-07-08 19:50:45 +0800

    first version

---
+++ schedule_alarm/schedule_json.js
@@ -0,0 +1,56 @@
+function f_return_json_schedule() {
+   // https://tableconvert.com/excel-to-json
+
+   return {"schedule":
+[
+  {
+    "time": "0800-0930",
+    "name": "晨練",
+    "note": "起床喝溫水（加檸檬）\n眼保健操 + 鳴天鼓36下\n經絡梳頭\n拍八虛和空抓手100下\n冥想",
+    "link": "晨練播放清單https://www.youtube.com/watch?v=HoSS0ZXOB3E&list=PLR-kEVmbxX8Qqa3zkBeSlCmo5MXDYnTcp\n早晨冥想https://www.bilibili.com/list/ml3583202884?bvid=BV1fp3yzvEQP&oid=114811505546359"
+  },
+  {
+    "time": "0930-1100",
+    "name": "早午餐",
+    "note": "備餐\n-早午餐食譜\n--碳水：地瓜(2根)/玉米(2根)\n--蛋白：雞蛋(2顆)/豆漿(1杯)\n--蛋白：魚肉(200g)/雞肉(200g)\n--蔬菜：青菜/紅蘿蔔/黃瓜/木耳(2拳)\n-晚餐食譜\n--碳水：地瓜/南瓜(1根)\n--蛋白：豆腐(300g)/魚(200g)\n--蔬菜：青菜/紅蘿蔔/黃瓜/木耳(1拳)\n吃早餐\n餐後散步（15分鐘）\n洗臉刷牙",
+    "link": ""
+  },
+  {
+    "time": "1100-1200",
+    "name": "復盤",
+    "note": "復盤昨日行情，記錄交易日誌\n確認與規劃當日行程",
+    "link": ""
+  },
+  {
+    "time": "1200-1500",
+    "name": "雜事",
+    "note": "工作/技能/娛樂\n每50分鐘休息10分鐘\n-遠眺 + 深蹲墊腳各30下 + 走動/喝水/拉伸",
+    "link": ""
+  },
+  {
+    "time": "1500-1700",
+    "name": "晚練",
+    "note": "拉伸：筋膜/髖/橫劈/直劈（60分鐘）\n重訓：胸/背/核心（40分鐘）\n有氧：快走/慢跑/跳繩/養生操（20分鐘）\n瑜珈：\n量體重",
+    "link": "晚練撥放清單https://www.youtube.com/watch?v=OOQruAZer3k&list=PLR-kEVmbxX8R3UBUyRiQEvmWKzXNP8CBc"
+  },
+  {
+    "time": "1700-1800",
+    "name": "晚餐",
+    "note": "吃晚餐\n餐後散步（15分鐘）\n洗澡",
+    "link": ""
+  },
+  {
+    "time": "1800-0000",
+    "name": "交易",
+    "note": "設置環境\n查看當天數據發布時間\n盤前行情分析與規劃\n盤中專注交易\n每50分鐘休息10分鐘\n-深蹲墊腳各30下 + 走動/喝水/拉伸",
+    "link": ""
+  },
+  {
+    "time": "0000-0800",
+    "name": "睡覺",
+    "note": "泡腳（15分鐘，水淹沒小腿肚，加艾葉/薑片）\n睡前瑜伽",
+    "link": "睡前撥放清單https://www.youtube.com/watch?v=jxzdSv7II-U&list=PLR-kEVmbxX8T14VDVavyOxoM3bvxZeDA2"
+  }
+]
+   };
+}
